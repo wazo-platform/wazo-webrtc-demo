06885fe0c695d94f769c527e54e9579affd00110
Sylvain Boily <sylvain@wazo.io>
2020-05-12 10:42:04 -0400

    keep the session on reload, add logout

--- src/authentication.js
+++ src/authentication.js
@@ -9,33 +9,63 @@ const authenticate = async (username, password, server) => {
   apiClient = new window['@wazo/sdk'].WazoApiClient({server});
 
   const session = await apiClient.auth.logIn({username, password}).catch(displayAuthError);
-  if (!session) {
-    return;
-  }
+  session.server = server;
+  setSessionOnStorage(session);
 
-  apiClient.setToken(session.token);
-  initializeWebRtc(server, session);
-  onLogin(session.uuid);
+  return session;
 };
 
 const openLoginModal = () => {
   $('#login-modal').modal({backdrop: 'static'});
 
-  $('#login-form').on('submit', e => {
+  $('#login-form').on('submit', async (e) => {
     e.preventDefault();
 
     $('#submit-login').prop('disabled', true);
-    authenticate($('#username').val(), $('#password').val(), $('#server').val());
+    const session = await authenticate(
+      $('#username').val(),
+      $('#password').val(),
+      $('#server').val()
+    );
+    onLogin(session);
   });
 };
 
-const onLogin = (uuid) => {
+const onLogin = (session) => {
   $('#submit-login').prop('disabled', false);
   $('#login-modal').modal('hide');
   $('#user').show();
+  $('#logout').on('click', () => {
+    removeSessionOnStorage();
+    location.reload();
+  });
 
-  setFullName(uuid);
+  apiClient = new window['@wazo/sdk'].WazoApiClient({server: session.server});
+  apiClient.setToken(session.token);
+  initializeWebRtc(session.server, session);
+  setFullName(session.uuid);
   resetMainDialer();
 };
 
-$(window).on('load', openLoginModal);
+const getSessionOnStorage = () => {
+  return JSON.parse(localStorage.getItem('session'));
+} 
+
+const setSessionOnStorage = (session) => {
+  localStorage.setItem('session', JSON.stringify(session));
+}
+
+const removeSessionOnStorage = (session) => {
+  localStorage.clear();
+}
+
+const launchPhone = () => {
+  const session = getSessionOnStorage();
+  if (!session) {
+    openLoginModal();
+  } else {
+    onLogin(session);
+  }
+}
+
+$(window).on('load', launchPhone);
