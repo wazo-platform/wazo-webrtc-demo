a1bb6ddf08db8cb35e0069c81a06c95685dbf14c
wazo <manon.gerray@gmail.com>
2021-06-10 11:09:00 +0200

    : :art: incoming call modal tweaks & calls handler code tests

--- src/dialer.js
+++ src/dialer.js
@@ -463,6 +463,39 @@ function addDialer(callSession, withVideo) {
   newDialer.appendTo($('#dialers'));
 }
 
+/* 
+  Trying to make the calls handler work 
+
+  function resume(callSessionId) {
+    const callSession = sessions[callSessionId];
+
+    Wazo.Phone.resume(callSession);
+
+    Object.keys(sessions).forEach((sessionId) => {
+      const cs = sessions[sessionId];
+      if (sessionId !== callSession.getId && !callSession.paused) {
+        Wazo.Phone.hold(cs);
+      } 
+    });
+  }
+*/
+
+function updateDialers() {
+  $('#dialers').html('');
+  $('#calls-handler').html('');
+
+  Wazo.Phone.hold(callSession);
+
+  Object.keys(sessions).forEach((sessionId) => {
+    const callSession = sessions[sessionId];
+    addDialer(callSession, callSession.cameraEnabled);
+    // $('#calls-handler').add(`
+    // <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+    // <label class="btn btn-outline-primary" for="btnradio1">${getNumber(callSession)}</label>
+    // `);
+  });
+}
+
 function updateDialers() {
   $('#dialers').html('');
 
