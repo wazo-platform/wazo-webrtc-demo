72c906ff53fa04b1ea9b08cf5e86c1f28297267a
Jeff Gauthier <code@bogue.ca>
2021-06-14 09:34:11 -0400

    Jeff's quick mods

--- tests/dialer.test.js
+++ tests/dialer.test.js
@@ -1,12 +1,38 @@
-import getNumber from "../src/dialer";
+import { getNumber, getStatus } from "../src/dialer";
+import { CallSession } from '@wazo/sdk';
 
-describe('Get user informations', function() {
+describe('Wazo', function() {
 
-  test('we get the user\'s name', () => {
-    const number = '8131';
+  describe('getNumber', () => {
+    test('we get the user\'s name', () => {
+      const number = '1234';
+      const callSession = new CallSession({
+        displayName: 'Donatien',
+        number,
+      });
 
-    expect((callSession.displayName).toBe('Donatien'));
+      const callSessionWithNumberOnly = new CallSession({
+        number,
+      });
+      
+      expect(getNumber(callSession)).toEqual('Donatien');
+      expect(getNumber()).toEqual(false);
+      expect(getNumber(callSessionWithNumberOnly)).toEqual(number);
+    });
+  })
+
+  describe('getStatus', () => {
+    const number = '1234';
+    const callSession = new CallSession({
+      paused: true,
+      number
+    });
+    const callSessionStatus = new CallSession({
+      number,
+      sipStatus: Wazo.Phone.SessionState.Initial
+    });
+    expect(getStatus()).toEqual(null);
+    expect(getStatus(callSession)).toEqual(`Call with ${number} on hold`);
+    expect(getStatus(callSessionStatus)).toEqual(`Calling ${number}...`);
   });
-
 })
-
