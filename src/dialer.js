7811785d808ceed52955e7329bd8cee896756353
wazo <manon.gerray@gmail.com>
2021-07-05 14:56:11 +0200

    :art: simple video display fixed

--- src/dialer.js
+++ src/dialer.js
@@ -282,53 +282,6 @@ function initializeMainDialer() {
   updateScenes();
 }
 
-// My beautiful Phone Timer, already deprecated 
-// function phoneTimer() {
-//   const start = new Date;
-//   const timer = $('.timer');
-//   timer.show();
-
-//   const getElapsedTimeInSeconds = (startTime) => {
-//     if (!startTime) {
-//       return 0;
-//     }
-//     return Math.trunc((Date.now() - startTime) / 1000);
-//   };
-  
-//   const formatTime = (secondsElapsed)  => {
-//     let secNum = parseInt(secondsElapsed, 10);
-//     let hours = Math.floor(secNum / 3600);
-//     let minutes = Math.floor((secNum - hours * 3600) / 60);
-//     let seconds = secNum - hours * 3600 - minutes * 60;
-  
-//     if (hours < 10) {
-//       hours = '0' + hours;
-//     }
-//     if (minutes < 10) {
-//       minutes = '0' + minutes;
-//     }
-//     if (seconds < 10) {
-//       seconds = '0' + seconds;
-//     }
-//     if (hours > 0) {
-//       return hours + ':' + minutes + ':' + seconds;
-//     }
-  
-//     return minutes + ':' + seconds;
-//   };
-
-//   countDown = setInterval(function() {
-//     timer.text(formatTime(getElapsedTimeInSeconds(start)));
-//   }, 250);
-
-//   return countDown;
-// }
-
-// function stopTimer()  {
-//   clearInterval(countDown);
-//   console.log(countDown);
-// }
-
 function addScene(callSession, withVideo) {
   const label = getNumber(callSession);
   const newScene = $('#root-scene')
